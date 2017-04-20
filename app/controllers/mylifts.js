8821bec5c29a92e61e1aa232c4cf43d9cfbd165e
cmdeveloped <cmwebdevs@gmail.com>
2017-04-20 14:29:40 -0500

    Updated Paper Select
    Filters to continue to exercise selection!

--- app/controllers/mylifts.js
+++ app/controllers/mylifts.js
@@ -9,6 +9,7 @@ export default Ember.Controller.extend({
     Ember.Object.create({goal: "Flexibility"})
   ]),
   muscleGroups: Ember.A([
+    Ember.Object.create({group: ''}),
     Ember.Object.create({group: "Chest"}),
     Ember.Object.create({group: "Back"}),
     Ember.Object.create({group: "Shoulders"}),
@@ -16,5 +17,30 @@ export default Ember.Controller.extend({
     Ember.Object.create({group: "Triceps"}),
     Ember.Object.create({group: "Biceps"}),
     Ember.Object.create({group: "Abdominals"})
-  ])
+  ]),
+  chosenGroups: Ember.A([]),
+  firstGroup: Ember.computed.filter('muscleGroups', function(item) {
+    return item !== this.get('chosenMuscleGroup2') && item !== this.get('chosenMuscleGroup3');
+  }),
+  secondGroup: Ember.computed.filter('muscleGroups', function(item) {
+    return item !== this.get('chosenMuscleGroup1') && item !== this.get('chosenMuscleGroup3');
+  }),
+  thirdGroup: Ember.computed.filter('muscleGroups', function(item) {
+    return item !== this.get('chosenMuscleGroup1') && item !== this.get('chosenMuscleGroup2');
+  }),
+  actions: {
+    changeGroups(groupNum, val) {
+      let key = "chosenMuscleGroup" + groupNum;
+      this.set(key, val);
+      this.get('chosenGroups').clear();
+      for (var i = 1; i < 4; i++) {
+        let tempKey = 'chosenMuscleGroup' + i;
+        let tempVal = this.get(tempKey);
+        if (tempVal && tempVal.get('group')) {
+          this.get('chosenGroups').addObject(tempVal);
+        }
+      }
+      this.get('muscleGroups').arrayContentDidChange(0); // fuck this horse shit
+    }
+  }
 });
